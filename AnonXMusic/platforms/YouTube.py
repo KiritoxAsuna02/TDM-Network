5cbee4f996e80d75cd13979f306840cb639f4f30
KiritoxAsuna02 <133078081+KiritoxAsuna02@users.noreply.github.com>
2025-06-14 17:32:59 +0530

    Update YouTube.py

--- AnonXMusic/platforms/YouTube.py
+++ AnonXMusic/platforms/YouTube.py
@@ -28,8 +28,8 @@ async def shell_cmd(cmd):
 
 
 async def get_stream_url(query, video=False):
-    api_url = "http://46.250.243.87:1470/youtube"
-    api_key = "1a873582a7c83342f961cc0a177b2b26"
+    api_url = "http://pragyansangeet.onrender.com"
+    api_key = "cbm_eb58b97e6a114b768d2a4ccc39be1cff"
     
     async with httpx.AsyncClient(timeout=60) as client:
         params = {"query": query, "video": video, "api_key": api_key}
